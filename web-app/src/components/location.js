f800fdb9135f405de501b3c8a9b018bfac80a468
aubstah24 <usui.aubrie@gmail.com>
2024-12-27 13:00:03 -1000

    location page content inserted

--- web-app/src/components/location.js
+++ web-app/src/components/location.js
@@ -1,12 +1,36 @@
-
-
+import { Divider, Image, Segment } from "semantic-ui-react";
+import location from "../images/location_filler.png";
 
 export const Location = () => {
-    return (
+  return (
+    <div classname="location-wrapper">
+      <h2>LOCATION</h2>
 
-        <div>
+      <Divider clearing />
 
+      <section>
+        <Image src={location} size="huge" alt={location} />
+        <p>
+          We are located at the intersection of Pensacola and King Street. Our
+          parking entrance is on the left side of Pensacola.
+        </p>
+      </section>
 
-        </div>
-    )
-}
+      <section>
+        <p>1003 Pensacola Street</p>
+        <p>Honolulu, HI 96814</p>
+      </section>
+
+      <h4>STORE HOURS:</h4>
+      <ul>
+        <li>Sunday: 12pm - 5pm</li>
+        <li>Monday: 11am - 3pm</li>
+        <li>Tuesday: 11am - 3pm</li>
+        <li>Wednesday: 11am - 3pm</li>
+        <li>Thursday: 11am - 3pm</li>
+        <li>Friday: 11am - 3pm</li>
+        <li>Saturday: 12pm - 5pm</li>
+      </ul>
+    </div>
+  );
+};
