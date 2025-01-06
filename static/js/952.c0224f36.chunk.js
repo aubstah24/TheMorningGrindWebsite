88f5c2e3d6ae05a91ef390a2d2a0bb228a6ce9b0
aubstah24 <usui.aubrie@gmail.com>
2025-01-06 09:39:47 -1000

    Updates

---
+++ static/js/952.c0224f36.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkweb_app=self.webpackChunkweb_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.c0224f36.chunk.js.map
